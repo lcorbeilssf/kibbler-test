168703f3670286bcb46c7f5f95542e0e178eedcc
lcorbeilssf <lcorbeil@softstackfactory.org>
2016-03-09 01:10:36 +0000

    kibbler-test

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,52 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic', 'ionic.contrib.ui.tinderCards'])
+
+.directive('noScroll', function() {
+
+  return {
+    restrict: 'A',
+    link: function($scope, $element, $attr) {
+
+      $element.on('touchmove', function(e) {
+        e.preventDefault();
+      });
+    }
+  };
+})
+
+.controller('CardsCtrl', function($scope, TDCardDelegate) {
+  var cardTypes = [
+    { image: '../img/ben.png' },
+    { image: '../img/dog1.jpg' },
+    { image: '../img/dog2.jpg' }
+  ];
+
+  $scope.cardDestroyed = function(index) {
+    $scope.cards.splice(index, 1);
+  };
+
+  $scope.addCard = function() {
+    var newCard = cardTypes[Math.floor(Math.random() * cardTypes.length)];
+    newCard.id = Math.random();
+    $scope.cards.unshift(angular.extend({}, newCard));
+  };
+  
+  $scope.cards = [];
+  for(var i = 0; i < 3; i++) $scope.addCard();
+})
+
+.controller('CardCtrl', function($scope, TDCardDelegate) {
+  $scope.cardSwipedLeft = function(index) {
+    console.log('LEFT SWIPE');
+    $scope.addCard();
+  };
+  $scope.cardSwipedRight = function(index) {
+    console.log('RIGHT SWIPE');
+    $scope.addCard();
+  };
+})
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,9 +68,10 @@ angular.module('starter', ['ionic'])
   
   $stateProvider
   
-  .state('landing', {
+  .state('swipe', {
     url: '/',
-    templateUrl: 'templates/landing.html',
+    templateUrl: 'templates/swipe.html',
+    controller: 'CardsCtrl'
   })
   .state('login', {
     url: '/login',
